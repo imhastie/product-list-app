35947ea0228a251761f73718ef8a6d6c66e327b2
Hasti <seethmione@gmail.com>
2026-07-29 15:54:42 +0330

    refactor: move API base URL to env

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://dummyjson.com";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function fetchProducts(query: string = ""): Promise<{ products: Product[] }> {
   const url = query
